4d8ae6c3c2dea23ac3e0dd77f8b5b7a5ce4f548f
Rogerio Goncalves <rogerlz@gmail.com>
2015-03-22 03:29:25 +0000

    workaround for debootstrap bug #732255

--- automation/bootstrap/debian7-bootstrap.sh
+++ automation/bootstrap/debian7-bootstrap.sh
@@ -6,6 +6,13 @@ srcroot=${srcroot:-$(dirname $(readlink -f "$0"))}
 
 . "$srcroot/bootstrap-lib.sh"
 
+# fixing bug https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=732255
+deb_add_repo wheezy "deb http://http.debian.net/debian wheezy main"
+deb_add_repo wheezy-security "deb http://security.debian.org/ wheezy/updates main"
+# endfix
+
+deb_add_repo wheezy-backports "deb http://http.debian.net/debian wheezy-backports main"
+
 deb_install apt-utils adduser wget cmake git devscripts pkg-config
 
 adduser --shell /bin/sh --disabled-password --home /home/leela --system leela
